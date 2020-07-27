46d1634f7b634c479d60c6bbe546e77b534abac6
yujun chen <ychen246@hawk.iit.edu>
2020-07-27 11:24:32 -0500

    Added Foreign Keys to tables

--- mssql/PizzaStoreDB.sql
+++ mssql/PizzaStoreDB.sql
@@ -7,6 +7,15 @@ go
 create schema Pizza;
 go
 
+create schema Users;
+go 
+
+create schema Orders;
+go
+
+create schema Stores;
+go 
+
 create table Pizza.Pizza
 (
 	PizzaId int not null primary key,
@@ -19,33 +28,10 @@ go
 create table Pizza.Topping
 (
 	ToppingId int not null primary key,
-	PizzaId int not null,
+	PizzaId int not null foreign key references Pizza.Pizza(PizzaId),
 	ToppingName nvarchar(250),
 )
 go
-
-create schema Orders;
-go
-
-create table Orders.Orders
-(
-	OrderId int not null primary key,
-	PurchaseDate datetime2(0) not null,
-	TotalPrice money not null,
-	UserId int not null,
-)
-go
-
-create table Orders.PizzaOrder
-(
-	PizzaOrderId int not null primary key,
-	PizzaId int not null,
-	OrderId int not null,
-)
-go
-
-create schema Users;
-go 
 
 create table Users.Users
 (
@@ -55,8 +41,22 @@ create table Users.Users
 )
 go
 
-create schema Stores;
-go 
+create table Orders.Orders
+(
+	OrderId int not null primary key,
+	PurchaseDate datetime2(0) not null,
+	TotalPrice money not null,
+	UserId int not null foreign key references Users.Users(UserId),
+)
+go
+
+create table Orders.PizzaOrder
+(
+	PizzaOrderId int not null primary key,
+	PizzaId int not null foreign key references Pizza.Pizza(PizzaId),
+	OrderId int not null foreign key references Orders.Orders(OrderId),
+)
+go
 
 create table Stores.Stores
 (
@@ -69,27 +69,27 @@ go
 create table Stores.StoreOrders
 (
 	StoreOrdersId int not null primary key,
-	StoreId int not null,
-	OrderId int not null,
+	StoreId int not null foreign key references Stores.Stores(StoreId),
+	OrderId int not null foreign key references Orders.Orders(OrderId),
 	Complete bit not null, 
 )
 go
 
-alter table Pizza.Topping
-	add constraint PizzaId foreign key references Pizza.Pizza(PizzaId)
+--alter table Pizza.Topping
+	--add constraint PizzaId foreign key references Pizza.Pizza(PizzaId)
 
-alter table Orders.Orders
-	add constraint UserId foreign key references Users.Users(UserId)
+--alter table Orders.Orders
+	--add constraint UserId foreign key references Users.Users(UserId)
 
-alter table Orders.PizzaOrder
-	add constraint PizzaId foreign key references Pizzas.Pizzas(PizzaId)
+--alter table Orders.PizzaOrder
+	--add constraint PizzaId foreign key references Pizzas.Pizzas(PizzaId)
 
-alter table Orders.PizzaOrder
-	add constraint OrderId foreign key references Orders.Orders(OrderId)
+--alter table Orders.PizzaOrder
+	--add constraint OrderId foreign key references Orders.Orders(OrderId)
 
-alter table Stores.StoreOrders 
-	add constraint StoreId foreign key references Store.Stores(StoreId)
+--alter table Stores.StoreOrders 
+	--add constraint StoreId foreign key references Store.Stores(StoreId)
 
-alter table Stores.StoreOrders
-	add constraint OrderId foreign key references Orders.Orders(OrderId)
+--alter table Stores.StoreOrders
+	--add constraint OrderId foreign key references Orders.Orders(OrderId)
 	
